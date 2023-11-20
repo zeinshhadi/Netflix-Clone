1e9a7fd98aa0112ea226bd8ebec2f39bd172673a
younes_damouna <younes.damouna@gmail.com>
2023-11-21 00:22:40 +0200

    styling edits for faq

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import Button from './components/Button';
-import GetStartedForm from './components/GetStartedForm';
-import Navbar from './components/Navbar';
-import './styles/utilities.css';
-import './styles/index.css';
-import FAQs from './components/FAQs';
+import Button from "./components/Button";
+import GetStartedForm from "./components/GetStartedForm";
+import Navbar from "./components/Navbar";
+import "./styles/utilities.css";
+import "./styles/index.css";
+import FAQs from "./components/FAQs";
 
-import { faqData } from './staticData/faq';
+import { faqData } from "./staticData/faq";
 
 function App() {
   return (
     <div className="App">
-
-      <div className='container'>
-      {/* <Navbar /> */}
+      <div className="container">
+        {/* <Navbar /> */}
 
         {/* <Button text="hello" /> */}
-       
 
-
-        <div className='container'>
-        {faqData.map((faq)=>{return(<>
-          <FAQs {...faq}/>
-        </>)})}
-
-      </div>
-      </div>
-
-     
-      <div className="container">
+        <div className="container">
+          <div className="primary-title text-white text-center">
+            Frequently Asked Questions
+          </div>
+          {faqData.map((faq) => {
+            return (
+              <>
+                <FAQs {...faq} />
+              </>
+            );
+          })}
+        </div>
         <GetStartedForm />
       </div>
 
+      
     </div>
   );
 }
