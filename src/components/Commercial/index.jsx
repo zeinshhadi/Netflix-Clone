71eef058263720b9d09cbe63ef61bcdbaeefec72
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-21 00:09:45 +0200

    added borders to each section

--- src/components/Commercial/index.jsx
+++ src/components/Commercial/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 const ComSection = ({ text, image, description, direction }) => {
   return (
-    <div className={`d-flex space-between ${direction}`}>
+    <div className={`d-flex space-between ${direction} border-t`}>
       <div className="left-section">
         <div className="sec-title">{text}</div>
         <div className="sec-desc">{description}</div>
