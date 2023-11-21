a8a70bf5ddc987baf281ad52f2dd8753a0c7ffb2
Zein Shhadi <zeinshhadi@hotmail.com>
2023-11-21 23:33:46 +0200

    cleaning for merge

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import Button from "./components/Button";
-import GetStartedForm from "./components/GetStartedForm";
-import Navbar from "./components/Navbar";
 import "./styles/utilities.css";
 import "./styles/index.css";
 import ComSection from "./components/Commercial";
-import tv from "../src/images/tv.png";
-import phone from "../src/images/mobile-0819.jpg";
-import pile from "../src/images/device-pile.png";
-import kids from "../src/images/kids.png";
+import tv from "./images/tv.png";
 function App() {
   const commercialSectionComponents = [
     {
       text: "Enjoy on your TV",
-      description: "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
       image: tv,
     },
     {
       text: "Download your shows to watch offline",
-      description: "Save your favorites easily and always have something to watch.",
-      image: phone,
+      image: "./images/mobile-0819.jpg",
     },
     {
       text: "Watch everywhere",
-      description: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.",
-      image: pile,
+      image: "./images/device-pile.png",
     },
     {
       text: "Create profiles for kids",
-      description:
-        "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
-      image: kids,
+      image: "../../images/kids.png",
     },
   ];
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        {commercialSectionComponents.map((element, index) => (
+          <ComSection
+            key={index}
+            text={element.text}
+            image={element.image}
+            direction={`${index % 2 === 0 ? "row" : "reverse-row"}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
