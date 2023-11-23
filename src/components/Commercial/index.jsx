ae79f66c5cc4cd30825eff4574f426b4f5001622
younes_damouna <younes.damouna@gmail.com>
2023-11-23 07:44:52 +0200

    make home page responsive

--- src/components/Commercial/index.jsx
+++ src/components/Commercial/index.jsx
@@ -3,12 +3,12 @@ import "./index.css";
 const ComSection = ({ text, image, description, direction }) => {
   return (
     <div className={`border-t`}>
-      <div className={`d-flex space-between ${direction} container`}>
-        <div className="left-section">
+      <div className={`d-flex wrap-sm space-between ${direction} container`}>
+        <div className="left-section ">
           <div className="sec-title primary-title">{text}</div>
           <div className="sec-desc">{description}</div>
         </div>
-        <div className="sec-image">
+        <div className="sec-image ">
           <img src={image} alt="" />
         </div>
       </div>
